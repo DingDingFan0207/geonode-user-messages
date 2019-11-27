8a825960bd6a8c8168349f6c676839409638d7f8
Dingding Fan <dingdingfan@catalyst.net.nz>
2019-11-27 13:35:58 +1300

    compatible with Django1.11

--- user_messages/tests/manage.py
+++ user_messages/tests/manage.py
@@ -1,12 +1,43 @@
-#!/usr/bin/env python
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+# #!/usr/bin/env python
+# from django.core.management import execute_manager
+# try:
+#     import settings # Assumed to be in the same directory.
+# except ImportError:
+#     import sys
+#     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+#     sys.exit(1)
+
+# if __name__ == "__main__":
+#     execute_manager(settings)
+
+
+import os
+import sys
+import tempfile
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+APP_ROOT = os.path.join(ROOT, '..')
+sys.path.append(APP_ROOT)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+
+# We do this here because settings.py has a tendency to be imported more than
+# once, in certain situations, and we only want one temporary test folder.
+MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'user_messages')
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(os.path.join(MEDIA_ROOT, 'test'))
+settings.MEDIA_ROOT = MEDIA_ROOT
+
 
 if __name__ == "__main__":
-    execute_manager(settings)
-
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(['user_messages'], verbosity=1)
+    if failures:
+        sys.exit(failures)
--- user_messages/tests/settings.py
+++ user_messages/tests/settings.py
@@ -2,6 +2,8 @@ DATABASE_ENGINE = "sqlite3"
 
 ROOT_URLCONF = "user_messages.urls"
 
+SECRET_KEY = "nothing important"
+
 TEMPLATE_CONTEXT_PROCESSORS = [
     "user_messages.context_processors.user_messages"
 ]
